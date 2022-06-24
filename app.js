c0484de8420f52a680c7c8632d6d1735b03659af
Kyle Glasper <ksglasper@gmail.com>
2022-06-24 16:39:23 -0500

    added buttons with only reset working for now. Clears all background images and resets gameBoard array to be all null.

--- app.js
+++ app.js
@@ -1,52 +1,62 @@
 const gameState = {
-    players: ['x', 'o'],
-    board: [
-      [null, null, null],
-      [null, null, null],
-      [null, null, null]
-    ]
-  }
-
+  players: ["x", "o"],
+  board: [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ],
+};
 
 classArrayLike = document.getElementsByClassName("gameTile");
 
 const gameGrid = Array.from(classArrayLike);
-let turnCounter = 0
-
+let turnCounter = 0;
 
 const gameArea = document.getElementById("gameBoard");
 gameArea.addEventListener("click", function (clickEvent) {
   if (clickEvent.target.matches(".gameTile")) {
-    if(clickEvent.target.matches('.x')){
-        return 'this square already has an x'
-    } else if(clickEvent.target.matches('.o')){  
-        return 'this square already has an o'
-    }else{
+    if (clickEvent.target.matches(".x") || clickEvent.target.matches(".o")) {
+    } else {
+      let currentPlayer = gameState.players[turnCounter % 2];
+      console.log(
+        `you just placed an ${currentPlayer} and the turn counter is ${turnCounter}!`
+      );
+      clickEvent.target.classList.toggle(currentPlayer);
 
-        
-    
-    let currentPlayer = gameState.players[turnCounter % 2]
-    console.log(`you just placed an ${currentPlayer} and the turn counter is ${turnCounter}!`)
-    clickEvent.target.classList.toggle(currentPlayer);
-
-    turnCounter++
-    console.log(turnCounter)
+      turnCounter++;
+      console.log(turnCounter);
     }
+  }
+});
+function updateGameBoard(num) {
+  let currentPlayer = gameState.players[turnCounter % 2];
+  const rowNumber = num.dataset.row;
+  const columnNumber = num.dataset.column;
+  gameState.board[rowNumber][columnNumber] = currentPlayer;
+
+  console.log([rowNumber, columnNumber]);
+  console.log(gameState.board);
 }
+const gameSettings = document.getElementById("gameSetting");
+gameSettings.addEventListener("click", function (clickEvent) {
+  if (clickEvent.target.matches(".boardReset")) {
+    const allGameTiles = document.getElementsByClassName("gameTile");
+
+    for (i = 0; i < allGameTiles.length; i++) {
+      allGameTiles[i].classList.remove("o");
+      allGameTiles[i].classList.remove("x");
+    }
+    for (j = 0; j < gameState.board.length; j++) {
+      for (k = 0; k < gameState.board[j].length; k++) {
+        gameState.board[j][k] = null;
+      }
+    }
+
+    console.log(gameState.board);
+  }
 });
 
-console.log(gameGrid)
+console.log(gameGrid);
 
 //Code for checking if the game has been won
-
-
-
-//Code for 
-
-// function moleTimer () {
-//     const randomHoleIndex = Math.floor(Math.random() * gameTile.length);
-
-//     gameTile[randomHoleIndex].classList.toggle("x");
-//   }
-  
-//   let myInterval = setInterval(moleTimer, 1000 );
+// alert(num.innerHTML + " is " + [rowNumber,columnNumber] +".");
