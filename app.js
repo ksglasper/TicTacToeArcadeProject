0a20221abe0154dc90bb4623b52596871b41ad48
Kyle Glasper <ksglasper@gmail.com>
2022-06-26 00:15:13 -0500

    created easy computer that makes random picks

--- app.js
+++ app.js
@@ -7,50 +7,41 @@ const gameState = {
   ],
 };
 const endResultTitle = document.getElementById("endResult");
-gameGrid = document.getElementsByClassName("gameTile");
+const gameGrid = document.getElementsByClassName("gameTile");
 const gameArea = document.getElementById("gameBoard");
 const gameSettings = document.getElementById("gameSetting");
-const playerOne = document.getElementById('playerOne')
-const playerTwo = document.getElementById('playerTwo')
-const playerArea = document.getElementById('playerNames')
-
-const playerOneNameSpan = document.getElementById('playerOneName')
-const playerTwoNameSpan = document.getElementById('playerTwoName')
-let playerOneName = ''
-let playerTwoName = ''
+const playerOne = document.getElementById("playerOne");
+const playerTwo = document.getElementById("playerTwo");
+// const playerArea = document.getElementById("playerNames");
+const playerOneNameSpan = document.getElementById("playerOneName");
+const playerTwoNameSpan = document.getElementById("playerTwoName");
+let playerOneName = "";
+let playerTwoName = "";
 
 let turnCounter = 0;
 let gameMode = 0;
-
-function nameSubmission(){
-  console.log(playerOne.value)
-  console.log(playerOneName)
-
-  if(playerOne.value === ''){
-  playerOneName = 'Player 1 : Computer'
-
-  }else{
-    playerOneName = `Player 1 : ${playerOne.value}`
-
+let gameWon = false
+console.log(gameGrid[0])
+function nameSubmission() {
+  if (playerOne.value === "") {
+    playerOneName = "Player 1 : Computer";
+  } else {
+    playerOneName = `Player 1 : ${playerOne.value}`;
   }
-  if(playerTwo.value === ''){
-    playerTwoName = 'Player 2 : Computer'
-    }else{
-      playerTwoName = `Player 2 : ${playerTwo.value}`
-    }
-  playerOneNameSpan.innerHTML = playerOneName
-  playerTwoNameSpan.innerHTML = playerTwoName
-  // playerArea.style.visibility = 'hidden'
-  
+  if (playerTwo.value === "") {
+    playerTwoName = "Player 2 : Computer";
+  } else {
+    playerTwoName = `Player 2 : ${playerTwo.value}`;
+  }
+  playerOneNameSpan.innerHTML = playerOneName;
+  playerTwoNameSpan.innerHTML = playerTwoName;
 }
 
-
 gameArea.addEventListener("click", function (clickEvent) {
-  
   if (clickEvent.target.matches(".gameTile")) {
-    if(gameMode === 0){
-      gameOverClassAdd()
-      alert('Please select a game mode')
+    if (gameMode === 0) {
+      gameOverClassAdd();
+      alert("Please select a game mode");
     }
     if (
       clickEvent.target.matches(".x") ||
@@ -63,11 +54,40 @@ gameArea.addEventListener("click", function (clickEvent) {
       clickEvent.target.classList.toggle(currentPlayer);
       updateGameBoard(clickEvent.target);
       turnCounter++;
-
       itsATie();
+      
+      if(gameWon){
+      }else if(gameMode === 1 && turnCounter !== 9){
+        endResultTitle.innerHTML = `Computer is making its move...`
+        setTimeout(computerMove, 1000)
+      }
+    
     }
+    console.log(gameState.board)
   }
 });
+
+function computerMove(){
+  let randomTileSelection = Math.floor(Math.random() * 9)
+  if(!gameWon){
+  while(
+    gameGrid[randomTileSelection].matches(".x") ||
+    gameGrid[randomTileSelection].matches(".o") ||
+    gameGrid[randomTileSelection].matches(".gameOver")
+  ) {
+    randomTileSelection = Math.floor(Math.random() * 9)
+    console.log(randomTileSelection)
+  }  
+    let currentPlayer = gameState.players[turnCounter % 2];
+
+    gameGrid[randomTileSelection].classList.toggle(currentPlayer);
+    updateGameBoard(gameGrid[randomTileSelection]);
+    turnCounter++;
+    endResultTitle.innerHTML = `Your Move!`;
+
+  }
+
+}
 
 gameSettings.addEventListener("click", function (clickEvent) {
   if (clickEvent.target.matches(".boardReset")) {
@@ -82,34 +102,47 @@ gameSettings.addEventListener("click", function (clickEvent) {
         gameState.board[j][k] = null;
       }
     }
-    playerOneNameSpan.innerHTML = ''
-    playerTwoNameSpan.innerHTML = ''
-    playerOne.style.visibility = 'visible'
-    playerTwo.style.visibility = 'visible'
 
     turnCounter = 0;
     gameOverClassRemoveAndText();
-
-  }
-
-  if (clickEvent.target.matches(".vsComputer")){
-    if(gameMode === 1){
-    } else{
-      boardReset()
-      gameOverClassRemoveAndText();
+    if (gameMode === 2) {
+      const randomStartingPerson = Math.floor(Math.random() * 2 + 1);
+      endResultTitle.innerHTML = `Player ${randomStartingPerson} goes first!`;
     }
-    gameMode = 1
-  }
-  
-  if (clickEvent.target.matches(".twoPlayer")){
-    if(gameMode === 2){
-    } else{
-      boardReset()
-      gameOverClassRemoveAndText();
+    if (gameMode === 1) {
+      endResultTitle.innerHTML = `Your Move!`;
     }
-    gameMode = 2
+    gameWon = false
   }
-  console.log(gameMode)
+
+  if (clickEvent.target.matches(".vsComputer")) {
+    if (gameMode === 1) {
+    } else {
+      boardReset();
+      gameOverClassRemoveAndText();
+      endResultTitle.innerHTML = `Your Move!`;
+    }
+    if (gameMode === 2 && playerTwoNameSpan.innerHTML !== "") {
+      playerTwoNameSpan.innerHTML = "Player 2 : Computer";
+    }
+    gameMode = 1;
+    gameWon = false
+  }
+
+  if (clickEvent.target.matches(".twoPlayer")) {
+    if (gameMode === 2) {
+    } else {
+      boardReset();
+      gameOverClassRemoveAndText();
+      const randomStartingPerson = Math.floor(Math.random() * 2 + 1);
+      endResultTitle.innerHTML = `Player ${randomStartingPerson} goes first!`;
+    }
+    if (gameMode === 1 && playerTwoNameSpan.innerHTML !== "") {
+      playerTwoNameSpan.innerHTML = "Player 2 : Jane Doe";
+    }
+    gameMode = 2;
+    gameWon = false
+  }
 });
 
 function updateGameBoard(num) {
@@ -124,14 +157,6 @@ function updateGameBoard(num) {
   }
 }
 
-
-
-
-
-
-
-
-
 function gameOverClassAdd() {
   for (i = 0; i < gameGrid.length; i++) {
     gameGrid[i].classList.add("gameOver");
@@ -142,9 +167,9 @@ function gameOverClassRemoveAndText() {
   for (i = 0; i < gameGrid.length; i++) {
     gameGrid[i].classList.remove("gameOver");
   }
-    endResultTitle.style.visibility = "hidden";
-    endResultTitle.style.transform = "none";
-    endResultTitle.style.transition = "none";
+  // endResultTitle.style.visibility = "hidden";
+  endResultTitle.style.transform = "none";
+  endResultTitle.style.transition = "none";
 }
 
 function endGame(string) {
@@ -169,6 +194,7 @@ function checkRowWin(row) {
   endGame(`${currentPlayer} has won! Congratulations!`);
   turnCounter = 0;
   gameOverClassAdd();
+  gameWon = true
 }
 
 function checkColumnWin(column) {
@@ -184,6 +210,8 @@ function checkColumnWin(column) {
   endGame(`${currentPlayer} has won! Congratulations!`);
   turnCounter = 0;
   gameOverClassAdd();
+  gameWon = true
+
 }
 
 function checkDiagonalWin() {
@@ -196,6 +224,9 @@ function checkDiagonalWin() {
     endGame(`${currentPlayer} has won! Congratulations!`);
     turnCounter = 0;
     gameOverClassAdd();
+    gameWon = true
+
+
   }
 
   if (
@@ -206,6 +237,7 @@ function checkDiagonalWin() {
     endGame(`${currentPlayer} has won! Congratulations!`);
     turnCounter = 0;
     gameOverClassAdd();
+    gameWon = true
   }
 }
 
@@ -214,8 +246,6 @@ function itsATie() {
     endGame(`It's a tie!`);
   }
 }
-
-
 
 function boardReset() {
   const allGameTiles = document.getElementsByClassName("gameTile");
